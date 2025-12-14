688278410d6d7ac4349012d3e5eeaa82976ccfbc
Vikrant Thigle <wicrant@gmail.com>
2025-12-14 16:55:29 +0530

    New UI for stocks-dashboard

--- app/stocks.py
+++ app/stocks.py
@@ -13,6 +13,8 @@ def fetch_stock_metrics(tickers):
         df["TR"] = df[["H-L", "H-PC", "L-PC"]].max(axis=1)
         df["ATR"] = df["TR"].rolling(window=14).mean()
         info = yf.Ticker(ticker).info
+        var_52WH =  percentchange((df["Close"].iloc[-1]), info.get("fiftyTwoWeekHigh", 0))
+        var_52WL = percentchange ((df["Close"].iloc[-1]), info.get("fiftyTwoWeekLow", 0))
         data.append({
             "Ticker": ticker,
             "current_price": round(df["Close"].iloc[-1], 2),
@@ -25,6 +27,11 @@ def fetch_stock_metrics(tickers):
             "support": round(df["Close"].rolling(20).min().iloc[-1], 2),
             "resistance": round(df["Close"].rolling(20).max().iloc[-1], 2),
             "Volume": int(df["Volume"].iloc[-1]),
-            "Market Cap": info.get("marketCap", 0)
+            "Market Cap": info.get("marketCap", 0),
+            "CP to 52WH": var_52WH,
+            "CP to 52WL": var_52WL
         })
     return data
+
+def percentchange(newvalue, oldvalue):
+    return round (((newvalue - oldvalue)*100/oldvalue),2)
